5845bc270e4551c1a320890d878e6a0b5f5b9f4d
Orlando Castillo <omaddoc@gmail.com>
2018-12-12 22:55:54 -0500

    start2

--- api/server.js
+++ api/server.js
@@ -13,6 +13,65 @@ server.get('/', (req, res) => {
   res.send(`Api running on port: ${port}`);
 });
 
+server.get('/api/notes', async (req, res) => {
+  knex('notes')
+    .then(notes => {
+      res.status(200).json(notes);
+    })
+    .catch(err => res.status(500).json(err));
+});
+
+server.post('/api/notes', (req, res) => {
+  const note = req.body;
+
+  knex('notes')
+    .insert(note)
+    .returning('id')
+    .then(ids => {
+      res.status(201).json(ids);
+    })
+    .catch(err => {
+      res.status(500).json({ error: 'Error creating note' });
+    });
+});
+
+server.get('/api/notes/:id', (req, res) => {
+  const { id } = req.params;
+
+  knex('notes')
+    .where({ id: id })
+    .then(note => {
+      res.status(200).json({ note });
+    })
+    .catch(err => res.status(500).json(err));
+});
+
+server.put('/api/notes/:id', (req, res) => {
+  const changes = req.body;
+  const { id } = req.params;
+
+  knex('notes')
+    .where({ id: id })
+    .update(changes)
+    .then(count => {
+      res.status(200).json({ count });
+    })
+    .catch(err => res.status(500).json(err));
+});
+
+server.delete('/api/notes/:id', (req, res) => {
+  const { id } = req.params;
+
+  knex('notes')
+    .where({ id: id })
+    .del()
+    .then(count => {
+      res.status(200).json({ count });
+    })
+    .catch(err => res.status(500).json(err));
+});
+
+// TEST
 server.get('/items', async (req, res) => {
   try {
     const items = await knex('items');
--- db/seeds/01-items.js
+++
@@ -1,7 +0,0 @@
-exports.seed = function(knex) {
-  return knex('items').insert([
-    { name: 'item 1' },
-    { name: 'item 2' },
-    { name: 'item 3' },
-  ]);
-};
